3dee3edc58b2b3286b26bcb73858a0a244f81c0e
Steven G. Harms <steven.harms@gmail.com>
2017-09-15 21:27:03 +0000

    Remove call to onclick: seems unused
    Browser is throwing an error here, but it doesn't
    seem to affect the gallery experience.

--- app/assets/javascripts/default.js
+++ app/assets/javascripts/default.js
@@ -40,12 +40,3 @@ if ($('#back-to-top').length) {
     });
 }
 
-// Gallery
-document.getElementById('gallery').onclick = function (event) {
-    event = event || window.event;
-    var target = event.target || event.srcElement,
-        link = target.src ? target.parentNode : target,
-        options = {index: link, event: event},
-        links = this.getElementsByTagName('a');
-   blueimp.Gallery(links, options);
-};
